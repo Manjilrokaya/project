ec774af17f4ce383696e57add70fdcc89c358bf9
Manjilrokaya <manjilrokaya0@gmail.com>
2025-02-14 07:02:26 +0545

    Add files via upload

---
+++ scriopt.js
@@ -0,0 +1,16 @@
+const flipBook = (elBook) => {
+    if (!elBook) return;
+    elBook.style.setProperty("--c", 0); // Set current page
+    elBook.querySelectorAll(".page").forEach((page, idx) => {
+      if (!page) return;
+      page.style.setProperty("--i", idx);
+      page.addEventListener("click", (evt) => {
+        if (evt.target.closest("a")) return;
+        const curr = evt.target.closest(".back") ? idx : idx + 1;
+        elBook.style.setProperty("--c", curr);
+      });
+    });
+  };
+  
+  document.querySelectorAll(".book").forEach(flipBook);
+  
